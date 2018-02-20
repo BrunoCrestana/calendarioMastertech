bb621ebecfd3cc72433474a9fa7deb0b98c34415
BrunoCrestana <brunozcrestana@gmail.com>
2018-02-19 22:40:40 -0300

    listaTarefas

--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista.component.scss']
 })
 export class ListaComponent implements OnInit {
-
+  tarefas = [{
+    nome:"",
+    feita:false
+  }];
   constructor() { }
 
   ngOnInit() {
   }
 
+  inserirTarefa(){
+    this.tarefas.push({nome:"", feita: false});
+  }
+
+  removerTarefa(i){
+    if(this.tarefas.length == 1){
+      this.inserirTarefa();
+    }
+    this.tarefas.splice(i,1);
+  }
 }
